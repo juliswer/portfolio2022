584c0e392341b5ec07c9a6faec8370e5fe33862d
Julian Swerdlin <julianswer36@gmail.com>
2022-03-12 20:19:22 -0300

    change

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,7 @@
-import '../styles/global.css'
-import { NextUIProvider } from "@nextui-org/react";
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-      <NextUIProvider>
-        <Component {...pageProps} />
-      </NextUIProvider>
-  );
+  return <Component {...pageProps} />
 }
 
 export default MyApp;
