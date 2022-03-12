0cab56e1a2997518baf60ae73bda499816f29dc6
Julian Swerdlin <julianswer36@gmail.com>
2022-03-12 20:09:11 -0300

    change

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import Image from 'next/image';
 
 const ErrorPage = () => {
 
@@ -9,7 +10,7 @@ const ErrorPage = () => {
             <title>404</title>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
-        <img src="https://miro.medium.com/max/1400/1*9m-WDdL_ji01bGbjEnutEw.gif" alt="404" />
+        <Image src="https://miro.medium.com/max/1400/1*9m-WDdL_ji01bGbjEnutEw.gif" alt="404" />
         <h1 className="text-3xl text-white">Hello world</h1>
     </div>
   );
