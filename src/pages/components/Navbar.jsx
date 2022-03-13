b1ed1567f93c946a23d70fcf3dc9d70ce75f0432
Julian Swerdlin <julianswer36@gmail.com>
2022-03-12 22:59:19 -0300

    404 page

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import logo from "../../assets/logo_dev-inc.png";
 import Image from "next/image";
+import Link from 'next/link'
+
 const Navbar = () => {
   return (
     <div>
       <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-800">
         <div className="container flex flex-wrap justify-between items-center mx-auto">
-          <a className="flex items-center">
-            <Image
-              src={logo}
-              className="mr-3 h-6 sm:h-10"
-              alt="Julian Logo"
-              width={90}
-              height={50}
-            />
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white ml-4">
-              Julian Swerdlin
-            </span>
+          <a className="flex items-center hover:cursor-pointer">
+            <Link href="/">
+            <div className="flex align-items-center">
+              <Image
+                src={logo}
+                className="mr-3 h-6 sm:h-10"
+                alt="Julian Logo"
+                width={90}
+                height={50}
+              />
+              <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white ml-4">
+                Julian Swerdlin
+              </span>
+            </div>
+            </Link>
           </a>
           <button
             type="button"
