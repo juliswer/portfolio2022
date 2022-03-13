8092ce9aa3d1588a529a445b5908afd95ae01058
Julian Swerdlin <julianswer36@gmail.com>
2022-03-13 11:39:48 -0300

    index changes

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -24,17 +24,13 @@ const Header = () => {
   }, []);
 
   return (
-    <div className="relative bg-blue-900">
+    <div className="relative bg-blue-900 mb-10">
       <div className="pt-10 mx-auto max-w-screen-xl pb-12 px-4 items-center lg:flex md:px-8">
-        <svg
-          className="absolute left-0"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1440 320"
-        >
+        <svg className="absolute left-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
           <path
             fill="#111827"
             fillOpacity="1"
-            d="M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,250.7C1248,256,1344,288,1392,304L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+            d="M0,64L48,80C96,96,192,128,288,154.7C384,181,480,203,576,213.3C672,224,768,224,864,234.7C960,245,1056,267,1152,256C1248,245,1344,203,1392,181.3L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
           ></path>
         </svg>
         <div className="space-y-4 flex-1 sm:text-center lg:text-left relative">
