d0652a6e5bcaea8c635f22e836f0ede9eae1e6b5
Julian Swerdlin <julianswer36@gmail.com>
2022-03-13 09:38:53 -0300

    index changes

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
+import Typed from "typed.js";
 
 const Header = () => {
+
+  const typeTarget = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(typeTarget.current, {
+      strings: ["Frontend Developer", "Backend Developer", "Fullstack Developer", "Mobile Developer"],
+      typeSpeed: 70,
+      loop: true
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
-    <div className="relative">
+    <div>
+      {/* <div className="relative">
       <img
         src="https://images.pexels.com/photos/249798/pexels-photo-249798.png?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
         className="absolute inset-0 object-cover w-full h-full bg-blend-multiply"
@@ -114,6 +131,69 @@ const Header = () => {
                 </form>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+    </div> */}
+      <div className="relative bg-blue-800">
+        <div className="absolute inset-x-0 bottom-0">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path
+              fill="#111827"
+              fill-opacity="1"
+              d="M0,192L48,192C96,192,192,192,288,202.7C384,213,480,235,576,229.3C672,224,768,192,864,181.3C960,171,1056,181,1152,176C1248,171,1344,149,1392,138.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+            ></path>
+          </svg>
+        </div>
+        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+          <div className="relative max-w-2xl sm:mx-auto sm:max-w-xl md:max-w-2xl sm:text-center">
+            <h2 className="mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
+              The quick, brown fox
+              <br className="hidden md:block" />
+              jumps over a
+              <span className="relative inline-block px-2">
+                <div className="absolute inset-0 transform -skew-x-12 bg-teal-accent-400" />
+                <span ref={typeTarget} className="font-bold text-amber-500" />
+              </span>
+            </h2>
+            <p className="mb-6 text-base text-indigo-100 md:text-lg">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae. explicabo. Sed ut perspiciatis unde omnis.
+            </p>
+            <form className="flex flex-col items-center w-full mb-4 md:flex-row md:px-16">
+              <input
+                placeholder="Email"
+                required
+                type="text"
+                className="flex-grow w-full h-12 px-4 mb-3 text-white transition duration-200 border-2 border-transparent rounded appearance-none md:mr-2 md:mb-0 bg-deep-purple-900 focus:border-teal-accent-700 focus:outline-none focus:shadow-outline"
+              />
+              <a
+                href="/"
+                className="inline-flex items-center justify-center w-full h-12 px-6 font-semibold tracking-wide text-teal-900 transition duration-200 rounded shadow-md md:w-auto hover:text-deep-purple-900 bg-teal-accent-400 hover:bg-teal-accent-700 focus:shadow-outline focus:outline-none"
+              >
+                Subscribe
+              </a>
+            </form>
+            <p className="max-w-md mb-10 text-xs tracking-wide text-indigo-100 sm:text-sm sm:mx-auto md:mb-16">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque.
+            </p>
+            <a
+              href="/"
+              aria-label="Scroll down"
+              className="flex items-center justify-center w-10 h-10 mx-auto text-white duration-300 transform border border-gray-400 rounded-full hover:text-teal-accent-400 hover:border-teal-accent-400 hover:shadow hover:scale-110"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="12"
+                height="12"
+                viewBox="0 0 12 12"
+                fill="currentColor"
+              >
+                <path d="M10.293,3.293,6,7.586,1.707,3.293A1,1,0,0,0,.293,4.707l5,5a1,1,0,0,0,1.414,0l5-5a1,1,0,1,0-1.414-1.414Z" />
+              </svg>
+            </a>
           </div>
         </div>
       </div>
