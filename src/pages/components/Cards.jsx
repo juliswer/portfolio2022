fcaba55a59c6436421a94a75b076ae80cc1314af
Julian Swerdlin <julianswer36@gmail.com>
2022-03-13 12:34:02 -0300

    index changes

--- src/pages/components/Cards.jsx
+++ src/pages/components/Cards.jsx
@@ -115,7 +115,7 @@ const Cards = () => {
           <a
             href="https://www.twitter.com/juliswer"
             target="_blank"
-            rel="noreferrer"
+            rel="noreferrer" 
             type="button"
             className="text-white bg-[#1da1f2] hover:bg-[#1da1f2]/90 focus:ring-4 focus:ring-[#1da1f2]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55 mr-2 mb-2"
           >
